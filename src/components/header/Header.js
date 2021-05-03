ea7374b9fe29e8081be5ab8bd39db6e1770238b3
israfil <israfil3019@gmail.com>
2021-05-04 02:51:46 +0300

    finish header part

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
+import "./Header.css"
 
 const Header = () => {
     return(
-        <div>
-            <h1>header</h1>
+        <div className="big-title">
+   
+            <h1>POPULAR TOUR PLACES</h1>
+            
         </div>
-    );
-};
+    )
+}
 
 export default Header;
