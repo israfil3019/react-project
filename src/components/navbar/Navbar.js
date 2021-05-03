0068eec8935bb8b39e0aaaa6385e9fba76bbaed2
israfil <israfil3019@gmail.com>
2021-05-04 02:39:06 +0300

    finish navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,16 @@
+import "./Navbar.css"
+
 const Navbar = () => {
     return(
-        <div>
-           <h1>navbar</h1>
+        <div className="navbar">
+            <a href="aboutUs">ABOUT US</a>
+            <a href="forYou">FOR YOU</a>
+            <a href="services">SERVICES</a>            
+            <a href="blog" className="active">BLOG</a>            
+            <a href="vlog">VLOG</a>            
+            <a href="contact">CONTACT</a>            
         </div>
-    );
-};
+    )
+}
 
 export default Navbar;
